e31988fa6c887143e2c89796bd2d2add65e76bdf
saaai <gopal.sai01@gmail.com>
2019-07-17 07:09:53 -0400

    Lab 2 Continuation

--- src/CustomerApp/CustomerApp.component.ts
+++ src/CustomerApp/CustomerApp.component.ts
@@ -13,11 +13,13 @@ export class CustomerComponent { //created class named as AppComponent
 
   CustomerModel : Customer = new Customer(); // creating customer model object, first it is variable name and then customer = new customer();
 
-  CustomerModels : Array<Customer> = new Array<Customer>();
+  CustomerModels : Array<Customer> = new Array<Customer>(); // this is for the collection of customer's data. So this is going to be an array.
+  //Array of Customer.
 
-  Add(){
-    this.CustomerModels.push(this.CustomerModel);
-    this.CustomerModel = new Customer();//clear UI
+  Add(){ //when Add button is called, we would like to add the CustomerModel data to CustomerModels 
+    this.CustomerModels.push(this.CustomerModel); // push the current customer model to this this.CustomerModels collection.
+    this.CustomerModel = new Customer();//clear UI, to clear the current customer UI and be ready with fresh customer object.
   }
+  
 }
  
